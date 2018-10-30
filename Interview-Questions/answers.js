a0e1e0f66ece2a3f0dc4b2a189cf2cd5f5ebc187
aneylon <arlen.m.neylon@gmail.com>
2018-10-29 21:41:01 -0600

    Add tests and answer for isUnique.

--- Interview-Questions/answers.js
+++ Interview-Questions/answers.js
@@ -1,1 +1,18 @@
-console.log('answers')
+/*
+  Create a function called 'isUnique'.
+  It should take a string as an argument.
+  If the string has any repeated characters it should return false.
+  If the string has no repeated characters it should return true.
+*/
+var isUnique = function(str) {
+  var count = {}
+
+  for(var i = 0; i < str.length; i++) {
+    if(count[str[i]] !== undefined){
+      return false
+    } else {
+      count[str[i]] = 1
+    }
+  }
+  return true
+}
--- Interview-Questions/tests.js
+++ Interview-Questions/tests.js
@@ -1,3 +1,6 @@
-QUnit.test('stuff', function(assert){
-  assert.ok(1 === 1, 'yep')
+QUnit.test('Is Unique', function(assert){
+  assert.ok(isUnique('abc'), 'Simple answer is simple.')
+  assert.ok(isUnique('Unique'), 'Unique is unique.')
+  assert.notOk(isUnique('abbc'), 'Doubles don\'t work.')
+  assert.notOk(isUnique('mississippi'), 'Too many repeats, don\'t check em all.')
 })
