e3081c606a522c1e21789bb4000e1cd3efaf3c7a
Arlen Neylon <aneylon@users.noreply.github.com>
2018-11-05 20:33:52 -0700

    Update answers.js

---
+++ JS-The-Good-Parts/02-Grammar/answers.js
@@ -0,0 +1,39 @@
+/*
+  Strings
+*/
+
+/*
+  Numbers
+*/
+
+/*
+  Arrays
+*/
+
+/*
+  Objects
+*/
+
+/*
+  Functions
+*/
+
+/*
+  For statements
+*/
+
+/*
+  While statements
+*/
+
+/*
+  Do While statements
+*/
+
+/*
+  Try statements
+*/
+
+/*
+  Switch statements
+*/
