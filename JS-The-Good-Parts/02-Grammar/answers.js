450fa27914a374493eb084d69fce518bcb6f1990
aneylon <arlen.m.neylon@gmail.com>
2018-11-06 19:29:12 -0700

    Block out Good parts grammar questions.

---
+++ JS-The-Good-Parts/02-Grammar/answers.js
@@ -0,0 +1,47 @@
+/*
+  Numbers
+*/
+
+/*
+  Strings
+*/
+
+/*
+  Arrays
+*/
+
+/*
+  Objects
+*/
+
+/*
+  if statments
+*/
+
+/*
+  while statements
+*/
+
+/*
+  do while statements
+*/
+
+/*
+  for statements // counted
+*/
+
+/*
+  for in statments
+*/
+
+/*
+  Switch case statments
+*/
+
+/*
+  try statements
+*/
+
+/*
+  Functions
+*/
