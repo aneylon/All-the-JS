f162b7fb03d455842436a19d11683e32a1b4ce90
aneylon <arlen.m.neylon@gmail.com>
2018-11-08 19:55:38 -0700

    Add todos for grammar section.

--- JS-The-Good-Parts/02-Grammar/answers.js
+++ JS-The-Good-Parts/02-Grammar/answers.js
@@ -1,17 +1,26 @@
 /*
   Numbers
+  Declare a new variable named 'myFavoriteNubmer' and set it to a number literal.
 */
 
 /*
   Strings
+  Declare a new variable named 'myNickName' and set it to a string literal.
+*/
+
+/*
+  Boolean
+  Declare a new variable named 'javaScriptIsAwesome' and set it to a boolean.
 */
 
 /*
   Arrays
+  Declare a new variable named 'myFavoriteNumbers' and set it equal to an array containing several numbers.
 */
 
 /*
   Objects
+  Declare a new variable named 'myPet' and set it equal to an object containing properties for name, age and species.
 */
 
 /*
